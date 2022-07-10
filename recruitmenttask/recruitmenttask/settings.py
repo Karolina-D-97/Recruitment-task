9d09b6ea4eec80f9de9ddfbb01dce88ba4904f0c
Karolina Duchna <72878154+Karolina-D-97@users.noreply.github.com>
2022-07-10 20:05:42 +0200

    Update settings.py

--- recruitmenttask/recruitmenttask/settings.py
+++ recruitmenttask/recruitmenttask/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dx77y7u!uz2que!ct*f+th(sp-v8z2u-lxf@jei7-g!8cbxon^'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
